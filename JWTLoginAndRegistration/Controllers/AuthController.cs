5bfb69fe5bb330792fd036661ee0108df1a7b310
izharcheema <108871168+izharcheema@users.noreply.github.com>
2022-07-15 17:02:57 +0500

    changed

--- JWTLoginAndRegistration/Controllers/AuthController.cs
+++ JWTLoginAndRegistration/Controllers/AuthController.cs
@@ -120,28 +120,6 @@ namespace JWTLoginAndRegistration.Controllers
 
             return jwt;
         }
-        //private string CreateToken(User user)
-        //{
-        //    List<Claim> claims = new List<Claim>
-        //    {
-        //        new Claim(ClaimTypes.Name, user.UserName),
-        //        new Claim(ClaimTypes.Role, "Admin")
-        //    };
-
-        //    var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
-        //        _configuration.GetSection("AppSettings:Token").Value));
-
-        //    var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
-
-        //    var token = new JwtSecurityToken(
-        //        claims: claims,
-        //        expires: DateTime.Now.AddDays(1),
-        //        signingCredentials: creds);
-
-        //    var jwt = new JwtSecurityTokenHandler().WriteToken(token);
-
-        //    return jwt;
-        //}
         private void CreatePasswordHash (string password,out byte[] passwordHash,out byte[] passwordSalt)
         {
             using(var hmac=new HMACSHA512())
